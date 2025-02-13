de1a91d8d2f3cc838a0afaf1d58d386591aacc9b
BurakTetikk <burktetk@gmail.com>
2025-02-13 07:12:20 +0300

    JDBC Connection Metadata

--- src/test/java/query_03/Query.java
+++ src/test/java/query_03/Query.java
@@ -18,6 +18,32 @@ public class Query {
 
         }
 
+        System.out.println("=============================================================");
+
+        resultSet = statement.executeQuery("select not_ortalamasi, cinsiyet, bolum from ogrenci_bilgiler order by not_ortalamasi, bolum");
+        int counter = 0;
+        while (resultSet.next()){
+            counter++;
+        }
+
+        System.out.println("Counter: " + counter);
+
+        ResultSetMetaData metaData = resultSet.getMetaData();
+
+        System.out.println("=======================================================");
+
+        System.out.println(metaData.getColumnCount());
+        System.out.println(metaData.getColumnName(1));
+        System.out.println(metaData.getColumnName(2));
+        System.out.println(metaData.getColumnName(3));
+        System.out.println(metaData.getColumnLabel(2));
+        System.out.println(metaData.getColumnType(2));
+        System.out.println(metaData.getColumnTypeName(2));
+        System.out.println(metaData.isNullable(3));
+        System.out.println("metaData = " + metaData); // org.postgresql.jdbc.PgResultSetMetaData@6356695f
+
+        System.out.println(metaData.getTableName(1));// tablodaki herbir sütunun hangi tablodan geldiğini gösterir
+
         resultSet.close();
         statement.close();
         connection.close();
